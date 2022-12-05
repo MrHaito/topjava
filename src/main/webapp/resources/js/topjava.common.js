cae3528bc43cbbe996337ef062d02712160bd717
o.klopskiy <abserber@gmail.com>
2022-12-05 13:13:43 +0300

    HW8

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -33,7 +33,7 @@ function deleteRow(id) {
 }
 
 function updateTable() {
-     $.get(ctx.ajaxUrl, function (data) {
+    $.get(ctx.ajaxUrl, function (data) {
         ctx.datatableApi.clear().rows.add(data).draw();
     });
 }
